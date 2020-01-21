d2c1e4d5a58653a5fb39a6b6dffb779e313e2c60
Alex Skreen, Alondra Warren and Dusty McCord <alondra@>
2020-01-21 11:12:09 -0800

    add second array, functionality to add/remove elements by clicking on either li

--- fav-things/js/scripts.js
+++ fav-things/js/scripts.js
@@ -1,23 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-var arr1 = [];    
+    var arr1 = [];
+    var arr2 = [];
 
-$("#submitButton").click(function(event){
-    event.preventDefault();
-    var favoriteThing = $("#favoriteThing").val();
-    arr1.push(favoriteThing);
-    $("#listUl").empty();
+    $("#submitButton").click(function (event) {
+        event.preventDefault();
+        var favoriteThing = $("#favoriteThing").val();
+        arr1.push(favoriteThing);
+        writeToPage();
 
-    arr1.forEach(function(item, index){
-        $("#listUl").append('<li id='+index+' class="list-time">' + item + ' </li>');
+
+        function writeToPage() {
+            $("#list1").empty();
+
+            arr1.forEach(function (item, index) {
+                $("#list1").append('<li id=' + index + ' class="list-time">' + item + ' </li>');
+            });
+            $("#list2").empty();
+            arr2.forEach(function (item, index) {
+                $("#list2").append('<li id=' + index + ' class="list-time">' + item + ' </li>');
+            });
+            $("#list1").children("li").click(function () {
+                arr2.push(arr1[this.id]);
+                arr1.splice(this.id, 1);
+                writeToPage();
+                console.log(this.id);
+            });
+
+            $("#list2").children("li").click(function () {
+                arr1.push(arr2[this.id]);
+                arr2.splice(this.id, 1);
+                writeToPage();
+                console.log(this.id);
+            });
+            console.log(arr1 + ' == ' + arr2 );
+        }
     });
-
-    $("#listUl").children("li").click(function(){
-        arr1.splice(this.id, 1);
-        console.log(this.id);
-    });
-
-    console.log(arr1);
-});
 
 });
